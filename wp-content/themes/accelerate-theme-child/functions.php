f1aec781a2ae06eeb5ceffe05582e5b52820a02f
Kristy Burge <kristyburge@gmail.com>
2016-06-24 08:12:10 -0500

    fixed typo in functions.php for custom field has_archive

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -49,7 +49,7 @@ function create_custom_post_types() {
                 'singular_name' => __( 'Case Study' )
                 ), 
             'public' => true, 
-            'has archive' => true, 
+            'has_archive' => true,
             'rewrite' => array ( 
                 'slug' => 'case-studies' 
                 ), 
