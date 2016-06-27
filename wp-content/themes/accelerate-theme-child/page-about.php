b5f2b26e6306ad16bafa7c7cb5a23c433b793c56
Kristy Burge <kristyburge@gmail.com>
2016-06-27 10:00:59 -0500

    added custom fields to about page for usability and updated button styles

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -4,36 +4,69 @@
  *
  */
 
-get_header(); ?>
+get_header(); 
+
+$company_name = get_field('company_name'); 
+$hero_text = get_field('hero_text'); 
+
+?>
 <div class="about-hero">
     <div class="about-text site-content">
-        <p><span class="accelerate">Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</p>
+        <p><span class="accelerate"><?php echo $company_name; ?></span> <?php echo $hero_text; ?></p>
     </div>
 </div>
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 			
-			<?php while ( have_posts() ) : the_post(); ?>
-			
+			<?php while ( have_posts() ) : the_post(); 
+            
+            $main_title = get_field('main_title'); 
+            $subtitle = get_field('subtitle'); 
+            
+            $service_1 = get_field('service_1'); 
+            $description_1 = get_field('description_1'); 
+            $image_1 = get_field('image_1'); 
+            
+            $service_2 = get_field('service_2'); 
+            $description_2 = get_field('description_2'); 
+            $image_2 = get_field('image_2'); 
+            
+            $service_3 = get_field('service_3'); 
+            $description_3 = get_field('description_3'); 
+            $image_3 = get_field('image_3'); 
+            
+            $service_4 = get_field('service_4'); 
+            $description_4 = get_field('description_4'); 
+            $image_4 = get_field('image_4'); 
 
+            $size = "full"; 
+            
+            $contact_text = get_field('contact_text'); 
+            $button_text = get_field('button_text'); 
+            $button_link = get_field('button_link'); 
+            
+            ?>
+			
             <div class="container">
                 <div class="row">
                    <div class="intro">
-                        <h4 class="about">Our Services</h4>
-                        <p class="lead">We take pride in our clients and the content we create for them.</p>
-
-                        <p class="lead">Here's a brief overview of our offered services.</p>
+                        <h4 class="about"><?php echo $main_title; ?></h4>
+                        <p class="lead"><?php echo $subtitle; ?></p>
                     </div><!-- .intro --> 
                 </div><!-- .row -->
 
                 <div class="row">
                     <div class="col-sm-4">
-                        <img class="alignnone wp-image-47355 size-full" src="http://localhost/accelerate/wp-content/uploads/2016/06/cs-arrow.png" alt="Content Strategy" width="221" height="218" />
+                        <?php 
+                            if ($image_1) { 
+                                echo wp_get_attachment_image( $image_1, $size ); 
+                            } 
+                        ?>
                     </div><!-- .col-sm-4 -->
                     <div class="col-sm-8">
                         <div class="description">
-                            <h2>Content Strategy</h2>
-                            <p>Bacon ipsum dolor amet short ribs doner jowl capicola. Rump drumstick pork pork loin. Shankle meatball beef ribs, short ribs leberkas meatloaf beef swine picanha. Ribeye ham hock jowl fatback picanha turkey, ground round shankle salami brisket.</p>
+                            <h2><?php echo $service_1; ?></h2>
+                            <p><?php echo $description_1; ?></p>
                         </div><!-- .description -->
 
                     </div><!-- .col-sm-8 -->
@@ -42,26 +75,34 @@ get_header(); ?>
                 <div class="row">
                     <div class="col-sm-8">
                         <div class="description">
-                            <h2>Influencer Mapping</h2>
-                            <p>Bacon ipsum dolor amet short ribs doner jowl capicola. Rump drumstick pork pork loin. Shankle meatball beef ribs, short ribs leberkas meatloaf beef swine picanha. Ribeye ham hock jowl fatback picanha turkey, ground round shankle salami brisket.</p>
+                            <h2><?php echo $service_2; ?></h2>
+                            <p><?php echo $description_2; ?></p>
 
                         </div><!-- .description -->
 
                     </div><!-- .col-sm-8 -->
                     <div class="col-sm-4">
-                        <img class="alignnone size-full wp-image-47357" src="http://localhost/accelerate/wp-content/uploads/2016/06/if-planet.png" alt="Influencer Mapping" width="221" height="221" />
+                        <?php 
+                            if ($image_2) { 
+                                echo wp_get_attachment_image( $image_2, $size ); 
+                            } 
+                        ?>
                     </div><!-- .col-sm-4 -->
 
                 </div><!-- .row -->
 
                 <div class="row">
                     <div class="col-sm-4">
-                        <img class="alignnone size-full wp-image-47358" src="http://localhost/accelerate/wp-content/uploads/2016/06/sms-like.png" alt="Social Media Strategy" width="221" height="221" />
+                        <?php 
+                            if ($image_3) { 
+                                echo wp_get_attachment_image( $image_3, $size ); 
+                            } 
+                        ?>
                     </div><!-- .col-sm-4 -->
                     <div class="col-sm-8">
                         <div class="description">
-                            <h2>Social Media Strategy</h2>
-                            <p>Bacon ipsum dolor amet short ribs doner jowl capicola. Rump drumstick pork pork loin. Shankle meatball beef ribs, short ribs leberkas meatloaf beef swine picanha. Ribeye ham hock jowl fatback picanha turkey, ground round shankle salami brisket.</p>
+                            <h2><?php echo $service_3; ?></h2>
+                            <p><?php echo $description_3; ?></p>
 
                         </div><!-- .description -->
 
@@ -71,27 +112,31 @@ get_header(); ?>
                 <div class="row">
                     <div class="col-sm-8">
                         <div class="description">
-                            <h2>Design &amp; Development</h2>
-                            <p>Bacon ipsum dolor amet short ribs doner jowl capicola. Rump drumstick pork pork loin. Shankle meatball beef ribs, short ribs leberkas meatloaf beef swine picanha. Ribeye ham hock jowl fatback picanha turkey, ground round shankle salami brisket.</p>
-
+                            <h2><?php echo $service_4; ?></h2>
+                            <p><?php echo $description_4; ?></p>
                         </div><!-- .description -->
 
                     </div><!-- .col-sm-8 -->
                     <div class="col-sm-4">
-                        <img class="alignnone size-full wp-image-47356" src="http://localhost/accelerate/wp-content/uploads/2016/06/dd-wand.png" alt="Design and Development" width="221" height="221" />
+                        <?php 
+                            if ($image_4) { 
+                                echo wp_get_attachment_image( $image_4, $size ); 
+                            } 
+                        ?>
                     </div><!-- .col-sm-4 -->
                 </div><!-- .row -->
             </div><!-- .container -->
-
-				<?php the_content(); ?>
+            
 			<?php endwhile; // end of the loop. ?>
 
         </div><!-- #content -->
     </div><!-- #primary -->
   	
 	<div class="call-to-action">
-        <p>Interested in working with us?</p>
-        <button type="button">Contact us</button>
+        <p><?php echo $contact_text; ?></p>
+        
+        <a href="<?php echo $button_link; ?>" class="button"><?php echo $button_text; ?></a>
+
     </div>
 
 
